2980b2b9aa5871589768c5bac0666f632cb1ee6c
yanni <kuriako1234@gmail.com>
2024-11-14 00:25:53 +0200

    Updates

--- Scaler/deleteall.sh
+++ Scaler/deleteall.sh
@@ -1,4 +1,4 @@
-microk8s kubectl delete deployment auto-scaler -n demo
-microk8s kubectl delete svc auto-scaler -n demo
-microk8s kubectl delete scaledobject bi-scaler    -n demo
+microk8s kubectl delete deployment auto-scaler -n app
+microk8s kubectl delete svc auto-scaler -n app
+microk8s kubectl delete scaledobject bi-scaler -n app
 
